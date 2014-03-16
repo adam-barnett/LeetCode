2cb5345b3df16ba3a297aeabf5dedd668244b7fa
adam-barnett <adam.barnett23@gmail.com>
2014-03-16 18:11:58 +0000

    added a solution for the maximum depth of a binary tree problem

---
+++ maximum-depth-of-binary-tree.py
@@ -0,0 +1,33 @@
+"""
+Given a binary tree, find its maximum depth.
+Leetcode problem can be found here:
+http://oj.leetcode.com/problems/maximum-depth-of-binary-tree/
+"""
+
+
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return an integer
+    def maxDepth(self, root, depth=0):
+        if root == None:
+            return depth
+        elif root.right == None and root.left == None:
+            depth += 1
+            return depth
+        else:
+            depth += 1
+            return self.bigger(self.maxDepth(root.right, depth),
+                                self.maxDepth(root.left, depth))
+        
+    def bigger(self, val1, val2):
+        if val1 > val2:
+            return val1
+        else:
+            return val2
